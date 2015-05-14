e811681148a529d460b6335ccc52ac9a945b0387
Vitosh <vitosh.doynov@gmail.com>
2015-05-14 21:19:17 +0300

    update 14.05

--- server.js
+++ server.js
@@ -8,24 +8,22 @@ app.set('view engine', 'jade');
 // routes
 app.get('/', function (req, res) {
   res.render('index');
-})
+});
 
 // listen for files: /post -> /views/post.jade
 app.get("/:fileName", function(req, res, next){
   if(req.params && req.params.fileName){
     var fileName = req.params.fileName.replace(".html","");
 
-    // if jade file exists
     if(fs.existsSync(__dirname+"/views/"+fileName+".jade")){
       res.render(fileName);
-    // if post is in posts
     } else {
       next();
     }
   } else {
     next();
   }
-})
+});
 
 var server = app.listen(3000, function () {
  
@@ -34,4 +32,4 @@ var server = app.listen(3000, function () {
  
   console.log('Example app listening at http://%s:%s', host, port);
  
-})
+});
